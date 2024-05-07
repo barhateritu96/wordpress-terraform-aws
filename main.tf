47252f328bb2fcdb6f5e82161b0b277491365bed
Ritu <103950525+barhateritu96@users.noreply.github.com>
2024-05-07 18:06:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,6 @@ module aws_wordpress {
     PUBLIC_KEY_PATH  = "./mykey-pair.pub" // key name for ec2, make sure it is created before terrafomr apply
     PRIV_KEY_PATH    = "./mykey-pair"
     instance_type    = "t2.micro"    //type of instance
-    instance_class   = "db.t2.micro" //type of RDS Instance
+    instance_class   = "db.t3.micro" //type of RDS Instance
     root_volume_size = 22
 }
